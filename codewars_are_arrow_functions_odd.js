d4a9a2d2c93f813ec16adb45d19ec63a1c54712a
Crew Spence <csspence123@gmail.com>
2021-01-15 07:08:39 -0600

    Wrote working solution.

--- codewars_are_arrow_functions_odd.js
+++ codewars_are_arrow_functions_odd.js
@@ -4,7 +4,9 @@ Time to test your basic knowledge in functions! Return the odds from a list:
 odds([1,2,3,4,5]) #=> [1,3,5]
 */
 
-function odds(values){
-  // arrow it
-  return values.filter( => );
+const odds = (values) => {
+  const modulo = (value) => {
+    return value % 2 !== 0 ? true : false;
+  }
+  return values.filter(modulo);
 }
